c4191895b6236c1424e97224df81eb5882991ef5
Jorge343 <jorge.gaitan.343@gmail.com>
2024-03-29 17:16:19 -0300

    agrego carpeta desactualizada

---
+++ azul/azul_prueba/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        boolean verdadero = true;
+        int b = 20;
+        int c = 20;
+        double a = 20.5;
+        string nombre = "Azul"; 
+ 
+
+        
+
+        
+        System.out.println("Hello, World!");
+    }
+
+}
